ed9e3afbc0893ed61751e211d5490c3422dcc807
maike cruz <maikesilva977@gmail.com>
2026-09-02 20:01:26 -0300

    Create firebaseConfig.js

---
+++ js/data/firebaseConfig.js
@@ -0,0 +1,22 @@
+// firebaseConfig.js
+// Configuração e inicialização do Firebase.
+// Esta configuração é pública (não é uma senha), então é seguro
+// mantê-la no repositório.
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBR9FNaNtvusTaEF3x8wlVAutgVBSV6o_M",
+  authDomain: "giro-app-96804.firebaseapp.com",
+  projectId: "giro-app-96804",
+  storageBucket: "giro-app-96804.firebasestorage.app",
+  messagingSenderId: "390682822432",
+  appId: "1:390682822432:web:6143c8078f5ef5c6543945"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
